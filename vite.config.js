e01f8f7cf99fea14a5126d9c982599ed199192f6
Lucas <lucascursos66@gmail.com>
2023-09-18 19:43:26 -0300

    aa

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https://gbs-404.github.io/ecomerce-project/",
+  base: "ecomerce-project/",
 });
