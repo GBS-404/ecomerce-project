24a6141cd78244361f480fb6e0086ffd77d8a05f
Lucas <lucascursos66@gmail.com>
2023-09-19 21:53:18 -0300

    asasd

--- src/content.jsx
+++ src/content.jsx
@@ -7,7 +7,7 @@ const Content = () => {
   return (
     <Router>
       <Routes>
-        <Route exact path="/ecomerce-project" element={<Store />}></Route>
+        <Route path="/ecomerce-project" element={<Store />}></Route>
         <Route path="/store" element={<Store />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
