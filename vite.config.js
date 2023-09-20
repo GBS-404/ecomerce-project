752a17c618a87cb4d19aae9ec3740b7b5cb92159
Lucas <lucascursos66@gmail.com>
2023-09-19 23:10:16 -0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Adicione um alias para resolver o módulo "react-icons/bs"
+      "react-icons/bs": "react-icons/bs",
+    },
+  },
 });
